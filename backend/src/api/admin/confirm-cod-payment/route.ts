e320b6954b2db553249948e0305d8450a90bf145
alejandro-myurban <alejandro@myurbanscoot.com>
2025-04-17 13:19:59 +0200

    added cod confirm-payment

---
+++ backend/src/api/admin/confirm-cod-payment/route.ts
@@ -0,0 +1,84 @@
+import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
+import { Modules } from "@medusajs/framework/utils";
+import jwt from "jsonwebtoken";
+
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  const token = req.query.token;
+  if (!token || typeof token !== "string") {
+    return res
+      .status(400)
+      .json({ success: false, message: "Token is required" });
+  }
+
+  try {
+    // Verificar el token JWT
+    const configModule = req.scope.resolve("configModule");
+    const jwtSecret = configModule.projectConfig.http.jwtSecret;
+    const decoded = jwt.verify(token, jwtSecret) as { order_id: string };
+    console.log("Decoded JWT:", decoded);
+    const orderId = decoded.order_id;
+
+    // Obtener el payment_collection asociado con la orden
+    const query = req.scope.resolve("query");
+    const { data: orders } = await query.graph({
+      entity: "order",
+      filters: {
+        id: orderId,
+      },
+      fields: ["payment_collections.*", "payment_collections.payments.*"],
+    });
+
+    if (
+      !orders ||
+      !orders[0] ||
+      !orders[0].payment_collections ||
+      orders[0].payment_collections.length === 0
+    ) {
+      return res.status(404).json({
+        success: false,
+        message: "No se encontraron colecciones de pago para esta orden",
+      });
+    }
+
+    // Capturar el pago
+    const paymentModuleService = req.scope.resolve(Modules.PAYMENT);
+    // Después de obtener la colección de pagos
+    const paymentCollection = orders[0].payment_collections[0];
+
+    // Obtener los pagos directamente de la colección de pagos
+    const { data: paymentCollectionDetails } = await query.graph({
+      entity: "payment_collection",
+      filters: {
+        id: paymentCollection.id,
+      },
+      fields: ["payments.*"],
+    });
+
+    if (
+      !paymentCollectionDetails ||
+      !paymentCollectionDetails[0] ||
+      !paymentCollectionDetails[0].payments ||
+      paymentCollectionDetails[0].payments.length === 0
+    ) {
+      return res.status(404).json({
+        success: false,
+        message: "No se encontraron pagos para esta colección de pago",
+      });
+    }
+
+    // Capturar el pago
+    await paymentModuleService.capturePayment({
+      payment_id: paymentCollectionDetails[0].payments[0].id,
+    });
+    // Responder con éxito
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Error al confirmar el pago:", error);
+    return res.status(400).json({
+      success: false,
+      message: "Token inválido o error al procesar el pago",
+    });
+  }
+};
+
+export const AUTHENTICATE = false;
---
+++ backend/src/subscribers/cod-payment.ts
@@ -0,0 +1,67 @@
+import { generateJwtToken, Modules } from "@medusajs/framework/utils";
+import {
+  INotificationModuleService,
+  IOrderModuleService,
+} from "@medusajs/framework/types";
+import { SubscriberArgs, SubscriberConfig } from "@medusajs/medusa";
+import { EmailTemplates } from "../modules/email-notifications/templates";
+import { STORE_CORS } from "lib/constants";
+
+export default async function confirmCodPaymentHandler({
+  event: { data },
+  container,
+}: SubscriberArgs<any>) {
+  console.log("HOLA MUNDO COD", data.id);
+
+  const notificationModuleService: INotificationModuleService =
+    container.resolve(Modules.NOTIFICATION);
+  const orderModuleService: IOrderModuleService = container.resolve(
+    Modules.ORDER
+  );
+
+  const order = await orderModuleService.retrieveOrder(data.id, {
+    relations: ["items", "summary", "shipping_address"],
+  });
+  const shippingAddress = await (
+    orderModuleService as any
+  ).orderAddressService_.retrieve(order.shipping_address.id);
+
+  // Obtener la configuración JWT del contenedor
+  const configModule = container.resolve("configModule");
+  const jwtSecret = configModule.projectConfig.http.jwtSecret;
+
+  // Generar un token JWT usando la utilidad de Medusa
+  const token = generateJwtToken(
+    {
+      payment_id: data.id,
+      order_id: order.id,
+    },
+    {
+      secret: jwtSecret,
+      expiresIn: "24h",
+    }
+  );
+  console.log("storecors", STORE_CORS);
+
+  try {
+    await notificationModuleService.createNotifications({
+      to: order.email,
+      channel: "email",
+      template: EmailTemplates.INVITE_USER,
+      data: {
+        emailOptions: {
+          replyTo: "info@myurbanscoot.com",
+          subject: "Necesitamos tu confirmación",
+        },
+        inviteLink: `${STORE_CORS}/es/confirm-cod-payment?token=${token}`,
+        preview: "Tu pedido ya casi está listo...",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const config: SubscriberConfig = {
+  event: "order.placed",
+};
