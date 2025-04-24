5676b9e1e3854402a7d822c7fe4f1794d735d137
alejandro-myurban <alejandro@myurbanscoot.com>
2025-04-24 17:35:01 +0200

    fixed packagejson 2

--- storefront/src/lib/config.ts
+++ storefront/src/lib/config.ts
@@ -6,10 +6,21 @@ let MEDUSA_BACKEND_URL = "http://localhost:9000"
 if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
   MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
 }
+console.log(
+  "[build] NEXT_PUBLIC_MEDUSA_BACKEND_URL =",
+  process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
+)
+console.log(
+  "[build] NEXT_PUBLIC_SEARCH_ENDPOINT    =",
+  process.env.NEXT_PUBLIC_SEARCH_ENDPOINT
+)
+console.log(
+  "[build] MEILISEARCH_API_KEY           =",
+  process.env.MEILISEARCH_API_KEY
+)
 
 export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,
   debug: process.env.NODE_ENV === "development",
   publishableKey: process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY,
 })
-
